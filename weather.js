411b9f27409865a330a4a80933b2699c7377a979
Kyle Sweigart <kasweigart@gmail.com>
2020-02-01 09:47:12 -0500

    added shadowing

--- weather.js
+++ weather.js
@@ -1,3 +1,14 @@
-const search = document.getElementsByClassName("search");
-console.log(search);
+document.getElementById("search").addEventListener("keypress", inputWeather);
 
+function inputWeather(e) {
+    if (e.keyCode === 13) {
+        console.log(test)
+    }
+}
+
+test = "Hello"
+
+api = {
+    key: "0051bc9e33a470267757d07b63bf6e95",
+    url: "http://api.openweathermap.org/data/2.5/"
+}
